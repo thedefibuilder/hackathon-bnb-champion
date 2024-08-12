8f6c1464c1a073cad08030dd002ed9bd3681c992
Rolandisdev <rolandisdev@gmail.com>
2024-08-12 22:00:59 +0300

    feat: header added

---
+++ src/components/common/header/logo.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { ENavRoutesPath } from "@/lib/routes/nav";
+
+export function LogoWithTitle() {
+  return (
+    <Link href={ENavRoutesPath.home}>
+      <Image
+        src="/logo-title-light.png"
+        alt="DeFi Builder's logo"
+        width={190}
+        height={30}
+        className="hidden dark:block"
+      />
+      <Image
+        src="/logo-title-light.png"
+        alt="DeFi Builder's logo"
+        width={190}
+        height={30}
+        className="block dark:hidden"
+      />
+    </Link>
+  );
+}
+
+type TLogoProps = {
+  width?: number;
+  height?: number;
+};
+
+export function Logo({ width = 190, height = 30 }: TLogoProps) {
+  /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+  return (
+    <>
+      <Image
+        src="/logo-title-light.png"
+        alt="DeFi Builder's logo"
+        width={width}
+        height={height}
+        className="hidden dark:block"
+      />
+      <Image
+        src="/logo-title-light.png"
+        alt="DeFi Builder's logo"
+        width={width}
+        height={height}
+        className="block dark:hidden"
+      />
+    </>
+  );
+}
