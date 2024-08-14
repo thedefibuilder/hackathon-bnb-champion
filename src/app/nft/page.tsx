fc0b9e5e315aa006931ce179369abcc3ca0f9f49
Rolandisdev <rolandisdev@gmail.com>
2024-08-14 14:21:10 +0300

    feat: build fix

--- src/app/nft/page.tsx
+++ src/app/nft/page.tsx
@@ -1,7 +1,10 @@
 import NftA from "@/templates/nft/nft-a";
 import NftB from "@/templates/nft/nft-b";
-import NftC from "@/templates/nft/nft-c";
+import dynamic from "next/dynamic";
 
+const NftC = dynamic(() => import("@/templates/nft/nft-c"), {
+  ssr: false,
+});
 export default function Nft() {
   // return <NftA />;
   // return <NftB />;
--- src/app/token/page.tsx
+++ src/app/token/page.tsx
@@ -1,7 +1,10 @@
-import TokenFiasco from "@/templates/token/token-fiasco";
 import TokenTango from "@/templates/token/token-tango";
 import TokneTornado from "@/templates/token/token-tornado";
+import dynamic from "next/dynamic";
 
+const TokenFiasco = dynamic(() => import("@/templates/token/token-fiasco"), {
+  ssr: false,
+});
 export default function Token() {
   // return <TokenTango /> ;
   return <TokenFiasco />;
