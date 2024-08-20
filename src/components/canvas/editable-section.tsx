60aad91bbee42bcc6b1cd4bacef170ed3f7fbbc4
urataps <alexeluca.spataru@gmail.com>
2024-08-20 14:37:03 +0300

    feat: sortable canvas list

--- src/components/canvas/editable-section.tsx
+++ src/components/canvas/editable-section.tsx
@@ -1,5 +1,7 @@
-import { CircleXIcon } from "lucide-react";
 import React from "react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CircleXIcon, MoveIcon } from "lucide-react";
+import { CSS } from "@dnd-kit/utilities";
 
 type TEditablSectionProps = {
   id: string;
@@ -12,15 +14,41 @@ export default function EditableSection({
   children,
   onRemove,
 }: TEditablSectionProps) {
-  return (
-    <div
-      className={`flex min-h-[100px] items-center justify-center rounded-md border-2 p-4`}
-    >
-      {children}
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    setActivatorNodeRef,
+  } = useSortable({
+    id,
+  });
 
-      <button className="right-2 top-2" onClick={() => onRemove(id)}>
-        <CircleXIcon className="h-6 w-6 fill-destructive" />
-      </button>
-    </div>
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <li ref={setNodeRef} style={style} {...attributes}>
+      <div
+        className={`flex min-h-[100px] items-center justify-center rounded-md border-2 p-4`}
+      >
+        {children}
+
+        <button className="right-2 top-2" onClick={() => onRemove(id)}>
+          <CircleXIcon className="h-6 w-6 fill-destructive" />
+        </button>
+
+        <button
+          className="right-2 top-2"
+          ref={setActivatorNodeRef}
+          {...listeners}
+        >
+          <MoveIcon className="h-6 w-6" />
+        </button>
+      </div>
+    </li>
   );
 }
